83dd71d9e9226204e35f7321dd106ca1bf3084e5
luis coelho <luiscoelho18994@gmail.com>
2022-09-15 15:16:12 +0100

    Update-Counting-DisplayInputNumber-To

--- src/App.js
+++ src/App.js
@@ -1,17 +1,77 @@
-
+import React, {useState, useEffect} from 'react';
+//useEffect to save number on the page 
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(100);
+
+  //controlled input
+  //connect the value with state variable
+  const [inputVal, setInputVal] = useState("");
+
+  
+// fire once , on begining
+  useEffect(() => {
+    localStorage.getItem("counter"); 
+    }, []);
+
+
+//save Number 
+useEffect(() => {
+localStorage.setItem("counter", counter.toString()); 
+}, [counter]);
+
+
+//make the code more dry
+const setCounterValue = (value) => () =>{
+  setCounter(value)
+};
+
+//extrat onchange input
+
+const hadleInputChange = (e) => {
+  setInputVal(e.target.value);
+};
+
+const handleUpdateValue = () => {
+const value = Number(inputVal);
+if (isNaN(value)){
+  setInputVal("");
+}else{
+  setCounter(value);
+  setInputVal("");
+}
+}
+
   return (
     <div className="App">
      <h1> React Counter</h1>
-     <h2>1</h2>
+
+     <h2>{counter}</h2>
     <div>
-      <button>-1</button>
-     <button>reset</button>
-     <button>+1</button>
+      <button onClick={(setCounterValue)(counter - 1)} >-1</button>
+     <button onClick={(setCounterValue)(0)}>reset</button>
+     <button onClick={(setCounterValue)(counter + 1)} >+1</button>
      </div> 
+
+<div>
+  <input  onChange={hadleInputChange} value={inputVal} />
+  <button onClick={handleUpdateValue}>Update</button>
+</div>
+
+
+
     </div>
+
+
+
+   /*   <h2>{counter}</h2>
+    <div>
+      <button onClick={()=> setCounter(counter - 1)} >-1</button>
+     <button onClick={()=> setCounter(0)}>reset</button>
+     <button onClick={()=> setCounter(counter + 1)} >+1</button>
+     </div> 
+    </div> */
   );
 }
 
