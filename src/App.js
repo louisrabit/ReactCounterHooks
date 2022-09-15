5198b7b4947c205bab89f7b683fc180162461553
luis coelho <luiscoelho18994@gmail.com>
2022-09-15 13:33:01 +0100

    CounterInThePage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> React Counter</h1>
+     <h2>1</h2>
+    <div>
+      <button>-1</button>
+     <button>reset</button>
+     <button>+1</button>
+     </div> 
     </div>
   );
 }
